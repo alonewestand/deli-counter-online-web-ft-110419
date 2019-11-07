fa1583f5532ab0eecece7544ba3977fd41945fc8
Ken Goad <goadke@g.cofc.edu>
2019-11-07 19:13:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(deli, name)
 end
 
 def now_serving(deli)
-  deli.empty? puts "There is nobody waiting to be served!" : puts "Currently serving #{deli.first}."
+  deli.empty ? puts "There is nobody waiting to be served!" : puts "Currently serving #{deli.first}."
   deli.shift
   end
   
